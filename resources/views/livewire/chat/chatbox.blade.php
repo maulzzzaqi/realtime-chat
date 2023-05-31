3a2ae7c2e6837bfd8e21920c6275f2d60036e0ca
Althaf-Budiman <budimanaltaf687@gmail.com>
2023-05-31 14:46:40 +0700

    setup a structure for livewire blade

--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -1,3 +1,4 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    chatbox
 </div>
--- resources/views/livewire/chat/create-chat.blade.php
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -2,7 +2,7 @@
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Users') }}
         </h2>
     </x-slot>
 </div>
